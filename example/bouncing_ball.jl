234dc733138df9b5c5cea40e9ac053c9cfaaa59a
Derek Fan <derekfan.18.2002@gmail.com>
2025-03-25 23:24:48 -0400

    tuned example parameters

--- example/bouncing_ball.jl
+++ example/bouncing_ball.jl
@@ -10,7 +10,7 @@ system = SaltedDIRCOL.bouncing_ball()
 Q = 1e0 * diagm([1.0, 1.0, 0.0, 0.0])
 R = 1e-6 * I(system.nu)
 Qf = 1e3 * Q
-N = 100
+N = 50
 Δt = 0.01
 params = SaltedDIRCOL.ProblemParameters(
     SaltedDIRCOL.hermite_simpson, system, Q, R, Qf, N, Δt
@@ -19,16 +19,16 @@ params = SaltedDIRCOL.ProblemParameters(
 # Define transition sequence and terminal guard
 impact = system.transitions["impact"]
 sequence = [
+    SaltedDIRCOL.TransitionTiming(10, impact),
     SaltedDIRCOL.TransitionTiming(20, impact),
+    SaltedDIRCOL.TransitionTiming(30, impact),
     SaltedDIRCOL.TransitionTiming(40, impact),
-    SaltedDIRCOL.TransitionTiming(60, impact),
-    SaltedDIRCOL.TransitionTiming(80, impact),
 ]
 term_guard = impact.guard
 
 # Define reference trajectory and initial conditions
 xic = [0.0; 10.0; 10.0; 0.0]
-xgc = [10.0; 5.0; 0.0; 0.0]
+xgc = [5.0; 5.0; 0.0; 0.0]
 xrefs = repeat(xgc, N)
 urefs = zeros((N-1) * system.nu)
 
@@ -43,5 +43,9 @@ y0 = zeros(params.dims.ny)
 sol = SaltedDIRCOL.ipopt_solve(params, cb, y0)
 
 # Visualize
-SaltedDIRCOL.plot_2d_trajectory(params.dims, params.idx, (1,2), sol.x)
+SaltedDIRCOL.plot_2d_trajectory(
+    params.dims, params.idx, (1,2), sol.x;
+    xlim = (0, 5),
+    ylim = (0, 10)
+)
 nothing
